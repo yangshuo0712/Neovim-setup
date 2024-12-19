b0ade087c6bb4d507f7ffce6de9f66c2a0fbfd8b
ys <yshuo1209@gmail.com>
2024-12-19 20:28:20 +0800

    add flash.lua

--- lua/setup/plugins/nvim-treesiter-textobjects.lua
+++ lua/setup/plugins/nvim-treesiter-textobjects.lua
@@ -16,9 +16,18 @@ return
                         ["ic"] = "@class.inner",
                         ["ap"] = "@parameter.outer",
                         ["ip"] = "@parameter.inner",
-                    }
-                }
-            }
+                    },
+                },
+                lsp_interop = {
+                    enable = true,
+                    border = "none",
+                    floating_preview_opts = {},
+                    peek_definition_code = {
+                        ["<leader>df"] = "@function.outer",
+                        ["<leader>dF"] = "@class.inner",
+                    },
+                },
+            },
         }
     end
 }
