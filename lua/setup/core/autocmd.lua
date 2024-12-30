bdca2fb12e43a1497dff0441d606d87882b24bfa
yangshuo0712 <yshuo1209@gmail.com>
2024-12-30 23:34:51 +0800

    2024.12.30

--- lua/setup/core/autocmd.lua
+++ lua/setup/core/autocmd.lua
@@ -5,26 +5,3 @@ vim.api.nvim_create_autocmd('TermOpen', {
         set_terminal_keymaps()
     end,
 })
-
-vim.api.nvim_create_autocmd("BufWinEnter", {
-    pattern = "*",
-    callback = function()
-        if vim.bo.filetype == "copilot-chat" then
-            vim.cmd("Copilot enable")
-            vim.g.copilot_enabled = true
-            vim.notify("Copilot enabled.", vim.log.levels.INFO)
-        end
-    end,
-})
-
-vim.api.nvim_create_autocmd("BufWinLeave", {
-    pattern = "*",
-    callback = function()
-        if vim.bo.filetype == "copilot-chat" then
-            vim.cmd("Copilot disable")
-            vim.g.copilot_enabled = false
-            vim.notify("Copilot disabled.", vim.log.levels.INFO)
-        end
-    end,
-})
-
